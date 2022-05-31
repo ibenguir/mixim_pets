af5cbccc21509bbf0372a08054bf84b00145f864
iness <ibenguir@esat.kuleuven.be>
2022-05-31 20:13:14 +0200

    Fig_3, fig_5, fig_6

--- main.py
+++ main.py
@@ -20,14 +20,14 @@ def main(arg):
     n_clients = int(config['DEFAULT']['n_clients'])
     lambda_c =  float(config['DEFAULT']['lambda_c'])
     #For Stratified Topology
-    n_layer = arg #int(config['TOPOLOGY']['n_layers'])
+    n_layer = 3
     n_mix_per_layer = int(config['TOPOLOGY']['l_mixes_per_layer'])
     total_n_mixes = n_layer * n_mix_per_layer
 
-    mu = 0.001 #(int(config['TOPOLOGY']['E2E']) - (n_layer + 1)*0.05)/n_layer
+    mu = 1
 
     # Threat Model
-    corrupt_mixes = 0 #rate * total_n_mixes
+    corrupt_mixes = arg
     balanced_corruption = bool(config['THREATMODEL']['balanced_corruption'])
     # propagation delays
     type = config['PROPAGATION']['type']
@@ -39,9 +39,17 @@ def main(arg):
     multiple_hops_dummies = dummies_vars.getboolean('multiple_hop_dummies')
     rate_mix_dummies =float(dummies_vars['rate_mix_dummies'])
 
-
-
-    weights = Weights(n_layer, n_mix_per_layer)
+    routing_type = config['NODES_SELETION']['routing_weights']
+    if routing_type == "uniform":
+        weights = Weights(n_layer, n_mix_per_layer)
+    elif routing_type == "biased":
+        weights = []
+        w_mix_l1 = [0.4, 0.6/9,0.6/9,0.6/9,0.6/9,0.6/9,0.6/9,0.6/9,0.6/9,0.6/9,]
+        weights.append(w_mix_l1)
+        weights.append(w_mix_l1)
+        weights.append(w_mix_l1)
+    else:
+        print("error typing routing type. Please check ConfigFile.ini")
     simulation = Simulation(mix_type=mix_type, simDuration=50, rate_client=1/lambda_c, mu=mu, logging=True,
                             topology=topology,fully_connected= fully_connected, n_clients=n_clients,printing = True, routing=routing, n_layers=n_layer,
                             n_mixes_per_layer=n_mix_per_layer,corrupt= corrupt_mixes,propagation=type,unifrom_corruption= balanced_corruption,probability_dist_mixes=weights,client_dummies=client_dummies,rate_client_dummies = rate_client_dummies, link_based_dummies = link_dummies, multiple_hops_dummies = multiple_hops_dummies,rate_mix_dummies = rate_mix_dummies,
@@ -56,8 +64,8 @@ def main(arg):
     return [entropy, entropy_mean, entropy_median , entropy_q25]
 
 if __name__ == "__main__":
-    p = Pool(processes=4, maxtasksperchild=1)
-    param = [1,5,10,15]
+    p = Pool(processes=2, maxtasksperchild=1)
+    param = [0, 0.1]
     result = p.map(main,param, chunksize=1)
     table_entropy = []
     for item in result:
