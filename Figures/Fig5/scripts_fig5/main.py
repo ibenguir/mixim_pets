72a330a8b296e9c39dbf45eeb31420d58095b6fa
iness <ibenguir@esat.kuleuven.be>
2022-06-05 00:25:29 +0200

    Fig3 to Fig8

--- Figures/Fig5/scripts_fig5/main.py
+++ Figures/Fig5/scripts_fig5/main.py
@@ -4,10 +4,10 @@ from multiprocessing import Pool
 from util import Weights
 import configparser
 
-def main(layer):
+def main(arg):
 
     config = configparser.ConfigParser()
-    config.read('Scripts/ConfigFile.ini')
+    config.read('ConfigFile.ini')
 
     topology_vars = config['TOPOLOGY']
     topology = topology_vars['type']
@@ -20,14 +20,14 @@ def main(layer):
     n_clients = int(config['DEFAULT']['n_clients'])
     lambda_c =  float(config['DEFAULT']['lambda_c'])
     #For Stratified Topology
-    n_layer = layer
+    n_layer = arg
     n_mix_per_layer = int(config['TOPOLOGY']['l_mixes_per_layer'])
     total_n_mixes = n_layer * n_mix_per_layer
 
     mu = 1
 
     # Threat Model
-    corrupt_mixes = 0
+    corrupt_mixes = arg
     balanced_corruption = bool(config['THREATMODEL']['balanced_corruption'])
     # propagation delays
     type = config['PROPAGATION']['type']
@@ -39,10 +39,20 @@ def main(layer):
     multiple_hops_dummies = dummies_vars.getboolean('multiple_hop_dummies')
     rate_mix_dummies =float(dummies_vars['rate_mix_dummies'])
 
-
-
-    weights = Weights(n_layer, n_mix_per_layer)
-    simulation = Simulation(mix_type=mix_type, simDuration=250, rate_client=1/lambda_c, mu=mu, logging=True,
+    routing_type = config['NODES_SELETION']['routing_weights']
+    if routing_type == "uniform":
+        weights = Weights(n_layer, n_mix_per_layer)
+    elif routing_type == "biased":
+        weights = []
+        w_mix_l1 = [0.4, 0.6/9,0.6/9,0.6/9,0.6/9,0.6/9,0.6/9,0.6/9,0.6/9,0.6/9,]
+        weights.append(w_mix_l1)
+        weights.append(w_mix_l1)
+        weights.append(w_mix_l1)
+    else:
+        weights = []
+        print("error typing routing type. Please check ConfigFile.ini")
+    print(weights)
+    simulation = Simulation(mix_type=mix_type, simDuration=50, rate_client=1/lambda_c, mu=mu, logging=True,
                             topology=topology,fully_connected= fully_connected, n_clients=n_clients,printing = True, routing=routing, n_layers=n_layer,
                             n_mixes_per_layer=n_mix_per_layer,corrupt= corrupt_mixes,propagation=type,unifrom_corruption= balanced_corruption,probability_dist_mixes=weights,client_dummies=client_dummies,rate_client_dummies = rate_client_dummies, link_based_dummies = link_dummies, multiple_hops_dummies = multiple_hops_dummies,rate_mix_dummies = rate_mix_dummies,
                             Network_template=None)
@@ -57,7 +67,7 @@ def main(layer):
 
 if __name__ == "__main__":
     p = Pool(processes=4, maxtasksperchild=1)
-    param = [1, 5, 10, 15]
+    param = [1,5,10,15]
     result = p.map(main,param, chunksize=1)
     table_entropy = []
     for item in result:
