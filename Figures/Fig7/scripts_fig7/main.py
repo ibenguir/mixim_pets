f5e57092ce59e1603940093e99ba809364d72e8c
iness <ibenguir@esat.kuleuven.be>
2022-06-09 21:15:02 +0200

    Updated figures Updated Jupyter files

--- Figures/Fig7/scripts_fig7/main.py
+++ Figures/Fig7/scripts_fig7/main.py
@@ -24,10 +24,10 @@ def main(arg):
     n_mix_per_layer = int(config['TOPOLOGY']['l_mixes_per_layer'])
     total_n_mixes = n_layer * n_mix_per_layer
 
-    mu = (int(config['TOPOLOGY']['E2E']) - (n_layer + 1)*0.05)/n_layer
+    mu = (float(config['TOPOLOGY']['E2E']) - (n_layer + 1)*0.05)/n_layer
 
     # Threat Model
-    corrupt_mixes = arg
+    corrupt_mixes = int(0*total_n_mixes)
     balanced_corruption = bool(config['THREATMODEL']['balanced_corruption'])
     # propagation delays
     type = config['PROPAGATION']['type']
@@ -51,8 +51,7 @@ def main(arg):
     else:
         weights = []
         print("error typing routing type. Please check ConfigFile.ini")
-    print(weights)
-    simulation = Simulation(mix_type=mix_type, simDuration=250, rate_client=1/lambda_c, mu=mu, logging=True,
+    simulation = Simulation(mix_type=mix_type, simDuration=150, rate_client=1/lambda_c, mu=mu, logging=True,
                             topology=topology,fully_connected= fully_connected, n_clients=n_clients,printing = True, routing=routing, n_layers=n_layer,
                             n_mixes_per_layer=n_mix_per_layer,corrupt= corrupt_mixes,propagation=type,unifrom_corruption= balanced_corruption,probability_dist_mixes=weights,client_dummies=client_dummies,rate_client_dummies = rate_client_dummies, link_based_dummies = link_dummies, multiple_hops_dummies = multiple_hops_dummies,rate_mix_dummies = rate_mix_dummies,
                             Network_template=None)
@@ -66,10 +65,10 @@ def main(arg):
     return [entropy, entropy_mean, entropy_median , entropy_q25]
 
 if __name__ == "__main__":
-    p = Pool(processes=19, maxtasksperchild=1)
-    param = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+    p = Pool(processes=4, maxtasksperchild=1)
+    param = [1,2,3,4]
     result = p.map(main,param, chunksize=1)
     table_entropy = []
     for item in result:
-        table_entropy.append(item[0])
+        table_entropy.append(item[1])
     print("Entropy", table_entropy)
